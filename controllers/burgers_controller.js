f16fc445f9b3d8c73e64dd97279dcf1b2848344c
Chelley <michelle.owodunni@gmail.com>
2020-01-06 19:28:35 -0600

    html updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const burgerJs = require("burger.js");
-const router = express.Router();
+// const express = require("express");
+// const burgerJs = require("burger.js");
+// const router = express.Router();
 
 
-// Create the `router` for the app, and export the `router` at the end of 
-// your file
-module.exports = router;
+// // // Create the `router` for the app, and export the `router` at the end of 
+// // // your file
+// module.exports = router;
